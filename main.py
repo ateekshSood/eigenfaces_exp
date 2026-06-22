0ed84f53b05cbd7d5e4570334e383904143ec587
Ateeksh <ateesood@gmail.com>
2026-06-22 11:22:17 +0000

    reconstructed the face using pca eigen face exp:

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ ax[0].imshow(x_train[ 0 , :].reshape(np.sqrt(x_train.shape[1]).astype(int) ,np.s
 ax[0].axis('off')
 ax[0].set_title("Normal")
 
-ax[1].imshow([ 0 , :].reshape(np.sqrt(x_train_centered.shape[1]).astype(int) ,np.sqrt(x_train_centered.shape[1]).astype(int) , -1) , cmap='binary')
+ax[1].imshow(x_train[ 0 , :].reshape(np.sqrt(x_train_centered.shape[1]).astype(int) ,np.sqrt(x_train_centered.shape[1]).astype(int) , -1) , cmap='binary')
 ax[1].axis('off')
 ax[1].set_title("Centered")
 
@@ -63,3 +63,43 @@ print(eigenvectors.shape)
 
 
 eigenvalues.shape
+# %%
+
+# cartesian product
+# 
+from itertools import product
+
+fig , ax = plt.subplots(3 , 3)
+
+for w,(i,z) in enumerate(product(range(3) , range(3))):
+
+    ax[i , z].imshow(sorted_eigenvectors[: , w].reshape(64 , 64 , -1) , cmap="binary")
+    ax[i, z].axis('off')
+
+# %%
+
+
+#findiunig the k 
+
+
+k = np.argmax(np.cumsum(sorted_eigenvalues) / total_eigenvalues_sum > 0.95)
+print(k)
+# %%
+
+test_face = x_train[0]
+
+used_principal_components = sorted_eigenvectors[: , :k]
+
+
+compress = test_face @ used_principal_components
+compress.shape
+
+decompress = compress @ used_principal_components.T
+
+final_reconstructed_face = decompress + x_train_mean
+
+plt.imshow(final_reconstructed_face.reshape(64 , 64 , -1) , cmap='binary')
+plt.axis('off')
+plt.show()
+
+
